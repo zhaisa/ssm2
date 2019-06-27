76f7e823fccfdc61fd2413b83181a34443bab149
Administrator <whatever>
2019-06-27 14:06:41 +0800

    resolve 修改根路径不生效的问题，在启动项添加修改根目录

--- src/main/java/com/jenkins/Ssm2Application.java
+++ src/main/java/com/jenkins/Ssm2Application.java
@@ -1,5 +1,8 @@
 package com.jenkins;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +10,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Ssm2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Ssm2Application.class, args);
+//		SpringApplication.run(Ssm2Application.class, args);
+		SpringApplication application = new SpringApplication(Ssm2Application.class);
+		Map<String, Object> map = new HashMap<>();
+		map.put("server.servlet.context-path", "/mydemo");
+		map.put("server.port", "9528");
+		application.setDefaultProperties(map);
+		application.run(args);
 	}
 
 }
--- src/main/java/com/jenkins/controller/LoginController.java
+++ src/main/java/com/jenkins/controller/LoginController.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
